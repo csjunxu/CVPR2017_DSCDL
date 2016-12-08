c24e64a3b3fac8bd3758b56e74817826c3c2b881
XuJun <nankaimathxujun@gmail.com>
2016-12-08 14:36:55 +0800

    add coupled ODL

---
+++ Utilities/Coupled_ODL.m
@@ -0,0 +1,54 @@
+% Main Function of Coupled Orthogonal Dictionary Learning
+% Input:
+% A: Initial coupled sparse coefficient
+% Xc, Xn    : Image Data Pairs of two domains
+% Dc, Dn    : Initial Dictionaries
+% par : Parameters
+%
+% Output:
+% A: Output coupled sparse coefficient
+% Dc, Dn : Output Coupled Dictionaries
+
+function [A, Dc, Dn,f] = Coupled_ODL(Xc, Xn, Dc, Dn, A, par)
+
+%% parameter setting
+param.lambda        = 	    par.lambda1; % not more than 20 non-zeros coefficients
+param.lambda2       =       par.lambda2;
+param.mode          = 	    2;       % penalized formulation
+param.approx=0;
+param.K = par.K;
+param.L = par.L;
+param.iter=300;
+
+f = 0;
+
+%% Iteratively solving:
+% coefficients: A
+% dictionary: D
+
+for t = 1 : par.nIter
+    %% Updating A
+    f_prev = f;
+    A = mexLasso([Xn; Xc], [Dn; Dc], param);
+    
+    %% Updating Dn and Dc
+    [Un,~,Vn] = svd(Xn*A','econ');
+    Dn = Un*Vn';
+    [Uc,~,Vc] = svd(Xc*A','econ');
+    Dc = Uc*Vc';
+    
+    %% Find if converge (NEED MODIFICATION)
+    P1 = Xc - Dc * A;
+    P1 = P1(:)'*P1(:);
+    P2 = Xn - Dn * A;
+    P2 = P2(:)'*P2(:);
+    P3 = par.lambda1 *  norm(A, 1);
+    f = P1 + P2 + P3;
+    
+    %% if converge then break
+    if (abs(f_prev - f) / f < par.epsilon)
+        break;
+    end
+    fprintf('Energy %d: %d\n', t, f);
+end
+
