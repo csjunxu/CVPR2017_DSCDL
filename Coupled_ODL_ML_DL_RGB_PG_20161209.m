412add282ba8082d8aa818af15a18b809f1fdfc9
XuJun <nankaimathxujun@gmail.com>
2016-12-09 14:36:17 +0800

    add

--- Coupled_ODL_ML_DL_RGB_PG_20161209.m
+++ Coupled_ODL_ML_DL_RGB_PG_20161209.m
@@ -38,7 +38,6 @@ for cls = 1 : par.cls_num
         par.lambda1 = lambda1(L);
         %% Orthogonal Dictionary Learning
         [A, Dc, Dn, f] = Coupled_ODL_RGB_PG_DL(XC, XN, Dc, Dn, A, par);
-        %%
         XN = Dn*A;
         par.PSNR(cls, L+1) = csnr( XN*255, XC*255, 0, 0 );
         par.SSIM(cls, L+1) = cal_ssim( XN*255, XC*255, 0, 0 );
